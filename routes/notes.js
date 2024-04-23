48fa49c7b5cd6673bfec6e500fb3b19789d7bfd8
Erik <rainer.erik70@gmail.com>
2024-04-23 14:09:12 -0700

    got it now its modular, it need now to display the note and add the delete function to

---
+++ routes/notes.js
@@ -0,0 +1,59 @@
+const notes = require('express').Router();
+
+const id = require("../helper/idcreator");
+
+const fs = require("fs");
+
+// POST request to add a note
+notes.post('/', (req, res) => {
+    // Log that a POST request was received
+    console.info(`${req.method} request received to add a note`);
+  
+    // Destructuring assignment for the items in req.body
+    const { title, text } = req.body;
+  
+    // If all the required properties are present
+    if (title && text) {
+      // Variable for the object we will save
+      const newNotes = {
+        title,
+        text,
+        note_id: id(),
+      };
+  
+      // Obtain existing notes
+      fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        if (err) {
+          console.error(err);
+        } else {
+          // Convert string into JSON object
+          const parsedNotes = JSON.parse(data);
+  
+          // Add a new note
+          parsedNotes.push(newNotes);
+  
+          // Write updated notes back to the file
+          fs.writeFile(
+            './db/db.json',
+            JSON.stringify(parsedNotes, null, 4),
+            (writeErr) =>
+              writeErr
+                ? console.error(writeErr)
+                : console.info('Successfully updated notes!')
+          );
+        }
+      });
+  
+      const response = {
+        status: 'success',
+        body: newNotes,
+      };
+  
+      console.log(response);
+      res.status(201).json(response);
+    } else {
+      res.status(500).json('Error in posting review');
+    }
+  });
+
+  module.exports = notes;
--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const path = require("path");
-const id = require("./helper/idcreator");
-const fs = require("fs");
-// const db = require("./db/db.json");
+const api = require("./routes/index.js");
 
 const PORT = 3001;
 
@@ -14,6 +12,9 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static("public"));
 
+app.use("/api", api);
+
+
 app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, "/public/index.html"))
 );
@@ -21,58 +22,6 @@ app.get('/', (req, res) =>
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, "/public/notes.html"))
 );
-
-// POST request to add a note
-app.post('/api/notes', (req, res) => {
-  // Log that a POST request was received
-  console.info(`${req.method} request received to add a note`);
-
-  // Destructuring assignment for the items in req.body
-  const { title, text } = req.body;
-
-  // If all the required properties are present
-  if (title && text) {
-    // Variable for the object we will save
-    const newNotes = {
-      title,
-      text,
-      note_id: id(),
-    };
-
-    // Obtain existing notes
-    fs.readFile('./db/db.json', 'utf8', (err, data) => {
-      if (err) {
-        console.error(err);
-      } else {
-        // Convert string into JSON object
-        const parsedNotes = JSON.parse(data);
-
-        // Add a new note
-        parsedNotes.push(newNotes);
-
-        // Write updated notes back to the file
-        fs.writeFile(
-          './db/db.json',
-          JSON.stringify(parsedNotes, null, 4),
-          (writeErr) =>
-            writeErr
-              ? console.error(writeErr)
-              : console.info('Successfully updated notes!')
-        );
-      }
-    });
-
-    const response = {
-      status: 'success',
-      body: newNotes,
-    };
-
-    console.log(response);
-    res.status(201).json(response);
-  } else {
-    res.status(500).json('Error in posting review');
-  }
-});
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT}`)
